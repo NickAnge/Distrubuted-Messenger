d1cf60a13a7d377ecc3adcce5007f94b4a4de978
Nikos Angelopoulos <angelopoulosn2@gmail.com>
2020-06-02 14:30:32 +0300

    Added A GroupManager Gif

--- src/GroupThreads.java
+++ src/GroupThreads.java
@@ -29,7 +29,7 @@ public class GroupThreads {
                         groupManager.getActiveMembers().remove(groupManager.getActiveMembers().get(counter));
                         groupManager.printList(groupManager.getListOfGroupsIntoManager());
 
-                        if(groupManager.getActiveMembers().size()!= tim){
+                        if (groupManager.getActiveMembers().size() != tim) {
                             break;
                         }
                         continue;
@@ -37,46 +37,44 @@ public class GroupThreads {
                     } else if (MsgRequest.equals("NoMessage")) {
                         continue;
                     }
-                    String []MsgSplit =  MsgRequest.split(" ",2);
+                    String[] MsgSplit = MsgRequest.split(" ", 2);
                     System.out.println(MsgSplit[0]);
                     if (MsgSplit[0].equals("Leave")) {
                         int idGroup = Integer.parseInt(MsgSplit[1]);
 
-
                         GroupInfo group = groupManager.findMembersGroup(idGroup);
-
 
                         EachMemberInfo member = null;
                         for (int i = 0; i < group.getMembers().size(); i++) {
-                            if (group.getMembers().get(i).getAppSocket().equals(groupManager.getActiveMembers().get(counter))) {
+                            if (group.getMembers().get(i).getAppSocket()
+                                    .equals(groupManager.getActiveMembers().get(counter))) {
                                 member = group.getMembers().get(i);
                             }
                         }
                         if (member == null) {
                             continue;
                         }
-                        String leaveMsg = new String("This member left the Group: "+member.getName());
-                        //TSEKARW AN EINAI COORDINATOR AUTOS POU FEugei GIA NA TO ALAKSW
-                        if(group.getCoInfo().getCoMember().getMemberPort() == member.getMemberPort()){
+                        String leaveMsg = new String("This member left the Group: " + member.getName());
+                        // TSEKARW AN EINAI COORDINATOR AUTOS POU FEugei GIA NA TO ALAKSW
+                        if (group.getCoInfo().getCoMember().getMemberPort() == member.getMemberPort()) {
                             group.getMembers().remove(member);
                             group = groupManager.changeCoordinator(group);
-                        }
-                        else{
+                        } else {
                             group.getMembers().remove(member);
-//                            Message newView = new Message("Leave",group,leaveMsg);
-//                            groupManager.informTheGroup(group, newView, member.getName());
-//                            break;
+                            // Message newView = new Message("Leave",group,leaveMsg);
+                            // groupManager.informTheGroup(group, newView, member.getName());
+                            // break;
                         }
-//                        String msg = groupManager.getMsgFromSocket(groupManager.getActiveMembers().get(counter));
-//                        if(msg == null){
-//                            groupManager.getActiveMembers().remove(groupManager.getActiveMembers().get(counter));
-//                        }
+                        // String msg =
+                        // groupManager.getMsgFromSocket(groupManager.getActiveMembers().get(counter));
+                        // if(msg == null){
+                        // groupManager.getActiveMembers().remove(groupManager.getActiveMembers().get(counter));
+                        // }
                         if (group.getMembers().size() == 0) {
                             groupManager.getListOfGroupsIntoManager().remove(group);
                             break;
-                        }
-                        else {
-                            Message newView = new Message("Leave",group,leaveMsg);
+                        } else {
+                            Message newView = new Message("Leave", group, leaveMsg);
                             groupManager.informTheGroup(group, newView, member.getName());
                             break;
                         }
@@ -99,62 +97,64 @@ public class GroupThreads {
             }
         }
     }
-        static class RecieveNewApps extends Thread {
 
-            MulticastSocket MainThreadSocket;
-            public String MultiCastAddress = "230.0.0.0";
-            public int MultiCastPort = 4321;
-            GroupManager groupManager;
+    static class RecieveNewApps extends Thread {
 
-            public RecieveNewApps(GroupManager Manager) {
-                this.groupManager = Manager;
-            }
+        MulticastSocket MainThreadSocket;
+        public String MultiCastAddress = "230.0.0.0";
+        public int MultiCastPort = 4321;
+        GroupManager groupManager;
 
-            @Override
-            public void run() {
-                try {
-                    ServerSocket Tcp = new ServerSocket(0);
+        public RecieveNewApps(GroupManager Manager) {
+            this.groupManager = Manager;
+        }
 
-                    while (true) {
-                        System.out.println("Waiting For new Apps...");
-                        MainThreadSocket = new MulticastSocket(MultiCastPort);
-                        InetAddress group = InetAddress.getByName(MultiCastAddress);
-                        MainThreadSocket.joinGroup(group);
-                        byte[] msg = new byte[1024];
-                        DatagramPacket packet = new DatagramPacket(msg, msg.length);
+        @Override
+        public void run() {
+            try {
+                ServerSocket Tcp = new ServerSocket(0);
 
+                while (true) {
+                    System.out.println("Waiting For new Apps...");
+                    MainThreadSocket = new MulticastSocket(MultiCastPort);
+                    InetAddress group = InetAddress.getByName(MultiCastAddress);
+                    MainThreadSocket.joinGroup(group);
+                    byte[] msg = new byte[1024];
+                    DatagramPacket packet = new DatagramPacket(msg, msg.length);
 
-                        MainThreadSocket.receive(packet);
+                    MainThreadSocket.receive(packet);
 
-                        System.out.println("New app request connection...");
-                        String msg1 = new String(packet.getAddress() + " " + Tcp.getLocalPort());
-                        byte[] bytemsg = msg1.getBytes();
-                        System.out.println("Sennding the Tcp_info,port" + Tcp.getLocalPort());
-                        DatagramSocket UdpSocket = new DatagramSocket();
-                        DatagramPacket packet2 = new DatagramPacket(bytemsg, bytemsg.length, packet.getAddress(), packet.getPort());
+                    System.out.println("New app request connection...");
+                    String msg1 = new String(packet.getAddress() + " " + Tcp.getLocalPort());
+                    byte[] bytemsg = msg1.getBytes();
+                    System.out.println("Sennding the Tcp_info,port" + Tcp.getLocalPort());
+                    DatagramSocket UdpSocket = new DatagramSocket();
+                    DatagramPacket packet2 = new DatagramPacket(bytemsg, bytemsg.length, packet.getAddress(),
+                            packet.getPort());
 
-                        UdpSocket.send(packet2);
+                    UdpSocket.send(packet2);
 
+                    Tcp.setSoTimeout(1000);
+                    try {
+                        Socket AppCommunicationInfo = Tcp.accept();
+                        System.out.println("App accepted the communication");
+                        // System.out.println("Adress " +
+                        // AppCommunicationInfo.getInetAddress().getHostAddress() + "Port " +
+                        // AppCommunicationInfo.getPort());
+                        groupManager.getActiveMembers().add(AppCommunicationInfo);
 
-                        Tcp.setSoTimeout(1000);
-                        try {
-                            Socket AppCommunicationInfo = Tcp.accept();
-                            System.out.println("App accepted the communication");
-                            System.out.println("Adress " + AppCommunicationInfo.getInetAddress().getHostAddress() + "Port " + AppCommunicationInfo.getPort());
-                            groupManager.getActiveMembers().add(AppCommunicationInfo);
-
-                            synchronized (lock) {
-                                lock.notify();
-                            }
-                        } catch (SocketTimeoutException ex) {
-                            System.out.println("Propably App didnt receive the packet ... connection Lost");
-                            continue;
+                        synchronized (lock) {
+                            lock.notify();
                         }
+                    } catch (SocketTimeoutException ex) {
+                        System.out.println("Propably App didnt receive the packet ... connection Lost");
+                        continue;
                     }
-                } catch (IOException ex) {
-
-                    ex.printStackTrace();
                 }
+            } catch (IOException ex) {
+
+                ex.printStackTrace();
             }
         }
     }
+}
